f1c048b21082126f03833e8e73be184923676e48
Doğan Çeçen <sepeth@gmail.com>
2014-11-06 23:17:12 +0200

    Add benchmarks for contains method

--- benchmarks.py
+++ benchmarks.py
@@ -19,13 +19,14 @@ def timeit(name):
     print(t2 - t1)
 
 
-RANDOMLONGS = [random.randint(1, 500000) for i in range(10000)]
+RANDOMLONGS_E4 = [random.randint(1, 500000) for i in range(10000)]
+RANDOMLONGS_E3 = [random.randint(1, 500000) for i in range(1000)]
 
 
 def sort_nearly_sortedlist():
     s = []
     with timeit('append to a list and sort it each time'):
-        for i in RANDOMLONGS:
+        for i in RANDOMLONGS_E4:
             s.append(i)
             s.sort()
 
@@ -33,10 +34,27 @@ def sort_nearly_sortedlist():
 def sortedset_add():
     s = skiplist.SortedSet()
     with timeit('add operation of SortedSet'):
-        for i in RANDOMLONGS:
+        for i in RANDOMLONGS_E4:
             s.add(i)
+
+
+def list_contains():
+    with timeit('search inside list'):
+        for i in RANDOMLONGS_E3:
+            i in RANDOMLONGS_E4
+
+
+def sortedset_contains():
+    s = skiplist.SortedSet()
+    for i in RANDOMLONGS_E4:
+        s.add(i)
+    with timeit('search inside SortedSet'):
+        for i in RANDOMLONGS_E3:
+            i in s
 
 
 if __name__ == '__main__':
     sort_nearly_sortedlist()
     sortedset_add()
+    list_contains()
+    sortedset_contains()
